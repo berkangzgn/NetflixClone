ee32612591e7172a9eedbcb66aa76d29d4970599
berkangzgn <berkangzgn@protonmail.com>
2023-11-03 23:36:13 +0300

    SearchVC created

---
+++ NetflixClone/Controllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  NetflixClone
+//
+//  Created by Berkan Gezgin on 28.10.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
